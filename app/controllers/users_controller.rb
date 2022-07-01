4ca717316ea3d0404717c7cb33905af17b9897a5
Praveen Rakhecha <praveen.rakhecha@dianapps.com>
2022-07-01 14:41:09 +0530

    project setup and login feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+    skip_before_action :require_login, only: [:new, :create]
+    def show
+    end
+  
+    def index
+    end
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.create(user_params)
+      if @user.valid?
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        flash[:error] = "Error- please try to create an account again."
+        redirect_to new_user_path
+      end
+    end
+  
+    private 
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+  end
